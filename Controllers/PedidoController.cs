fac08482f4bf5f3bac34b96c7f96b37e3a8f84d7
Agata Cristina <agata.francisco@portalsesisp.org.br>
2023-11-14 14:01:21 -0300

    commit 1411

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -1,21 +1,39 @@
+using LaDoces2.Context;
 using LaDoces2.Models;
 using LaDoces2.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaDoces2.Controllers
 {
+    [Authorize]
     public class PedidoController : Controller
     {
         private readonly IPedidoRepository _pedidoR;
         private readonly Carrinho _carrinho;
-        public PedidoController(IPedidoRepository pedidoR, Carrinho carrinho)
+         private readonly AppDbContext _context;
+        public PedidoController(IPedidoRepository pedidoR, Carrinho carrinho, AppDbContext context)
         {
             _pedidoR = pedidoR;
             _carrinho = carrinho;
+            _context = context;
         }
         public IActionResult Checkout()
         {
-            return View();
+            UserAcount userlogado = _context.Users.FirstOrDefault(p => p.UserName == User.Identity.Name);
+            Pedido p = new Pedido
+            {
+                Email = userlogado.Email,
+                Nome = userlogado.Nome,
+                Endereco1 = userlogado.Endereco,
+                Numero = userlogado.Numero,
+                Bairro = userlogado.Bairro,
+                Cidade = userlogado.Cidade,
+                Estado = userlogado.Estado,
+                Cep = userlogado.Cep.ToString()
+                };
+            return View(p);
         }
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
@@ -49,10 +67,27 @@ namespace LaDoces2.Controllers
                 ViewBag.TotalPedido = _carrinho.GetCarrinhoCompraTotal();
                 //limpa o carrinho do cliente
                 _carrinho.LimparCarrinho();
+                ViewBag.UrlWhatsApp = EnviarMensagem(pedido.PedidoId);
                 //exibe a view com dados do cliente e do pedido
                 return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
             }
             return View(pedido);
+        }
+        public string EnviarMensagem(int pedidoId)
+        {
+            var pedido = _context.Pedidos.Include(p => p.PedidoItens).ThenInclude(p => p.Item).FirstOrDefault(p => p.PedidoId == pedidoId);
+
+            string numeroDestinatario = "+55Digite o DDD e numero tudo junto";
+            string mensagem = "Pedido: " + pedido.PedidoId.ToString() + "Cliente: "+pedido.Nome.ToString();
+            // Formate o número removendo caracteres não numéricos
+            string numeroFormatado = new string(numeroDestinatario.Where(char.IsDigit).ToArray());
+
+            // Construa o link do WhatsApp
+            string urlWhatsApp = $"https://web.whatsapp.com/send?phone=+{numeroFormatado}&text={Uri.EscapeDataString(mensagem)}";
+
+            Console.WriteLine(urlWhatsApp);
+            // Retorne a View que contém o script JavaScript
+            return urlWhatsApp;
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const cep = document.querySelector("#cep");
+const rua = document.querySelector('#rua');
+const bairro = document.querySelector('#bairro');
+const cidade = document.querySelector('#cidade');
+const estado = document.querySelector('#estado');
 
-// Write your JavaScript code.
+cep.addEventListener('blur', e=> {
+  consultaCep();
+});
+
+async function consultaCep(){
+
+          rua.value = ""
+          bairro.value = ""
+          cidade.value = ""
+          estado.value = ""
+ 
+  const value = cep.value;
+
+  const url = `https://viacep.com.br/ws/${value}/json/`;
+
+  await fetch(url)
+  .then(response => response.json())
+  .then( json => {
+       
+          rua.value = json.logradouro;
+          bairro.value = json.bairro;
+          cidade.value = json.localidade;
+          estado.value = json.uf;
+       
+    })
+    .catch( e => {
+      alert("Cep Errado! ou Falha no Servidor!")
+    })
+   
+  }
